e169f69d4bec8cbb657e94be113cb7f7373c00fb
LucasAliasElvennope <lucasmoreaupro0@gmail.com>
2025-06-02 11:08:58 +0200

    feat : add files to create my first components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BonjourComponent } from './bonjour/bonjour.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [BonjourComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'mon-app';
---
+++ src/app/bonjour/bonjour.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bonjour',
+  standalone: true,
+  templateUrl: './bonjour.component.html',
+  styleUrls: ['./bonjour.component.scss']
+})
+export class BonjourComponent { }
